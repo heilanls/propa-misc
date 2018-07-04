f8b110dc44d7cc11ec39b4ea56b55225fff0e39d
schuikla <33062005+schuikla@users.noreply.github.com>
2018-07-04 17:27:14 +0200

    Aufgabe 6.3
    Fertig, denke ich...

--- mast.hs
+++ mast.hs
@@ -8,30 +8,37 @@ data Flaggenmast =
    deriving (Show, Eq)
 
 beispielmast :: Flaggenmast
-beispielmast = Mast (Mast (Flagge Schwarz) (Flagge Schwarz)) Ende
+beispielmast = Mast Ende (Mast (Flagge Schwarz) (Flagge Schwarz))
 
 fold_mast :: (a -> a -> a) -> (Farbe -> a) -> a -> Flaggenmast -> a
 fold_mast m f e = fold_mast'
-      where 
-            fold_mast' Ende = e
+      where fold_mast' Ende = e
             fold_mast' (Flagge c) = f c
-            fold_mast' (Mast child1 child2) = m (fold_mast' child1) (fold_mast' child2)
-
+            fold_mast' (Mast links rechts) = m (fold_mast' links) (fold_mast' rechts)
 
 flaggen :: Flaggenmast -> Int
-flaggen mast = fold_mast m farbwert Ende
-            where m a b
-
---flaggenfarben :: Flaggenmast -> [Farbe]
+flaggen mast = fold_mast m f 0 mast
+      where m a b = a + b 
+            f c = 1
+            
+flaggenfarben :: Flaggenmast -> [Farbe]
+flaggenfarben mast = fold_mast m f [] mast
+      where m a b = a ++ b 
+            f c = c:[]
 
 farbwert :: Farbe -> Int
-farbwert farbe
-      | farbe == Rot = 1
-      | farbe == Schwarz = 5
-      | farbe == Gelb = 9
-      | farbe == Blau = 3
-      | farbe == Bunt = 0
+farbwert farbe 
+      |farbe == Rot = 1
+      |farbe == Schwarz = 5
+      |farbe == Gelb = 9
+      |farbe == Blau = 3
+      |farbe == Bunt = 0
 
---mastwert :: Flaggenmast -> Int
+mastwert :: Flaggenmast -> Int --funktioniert
+mastwert mast = fold_mast m farbwert (-1) mast
+      where m a b = a + b
 
---vielfarbig :: Flaggenmast -> Bool
+vielfarbig :: Flaggenmast -> Bool --funktioniert
+vielfarbig mast = fold_mast m f False mast
+      where m a b = if ( a == False && b == False ) then False else True
+            f c = if c == Bunt then True else False 
